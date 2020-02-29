c3a50dd0b4b6d155ca75571c6197001603d7ea31
Konrad Szpak <konradszpak67@gmail.com>
2020-02-29 20:50:51 +0000

    Add project files.

---
+++ Validations/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Validations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            double quotient;
+            bool isValid;
+
+            do
+            {
+                try
+                {
+                    isValid = true;
+
+                    Console.WriteLine("Enter the 1 number: ");
+                    num1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the 2 number: ");
+                    num2 = int.Parse(Console.ReadLine());
+
+                    quotient = Divide(num1, num2);
+
+                }
+                catch (ArithmeticException myError)
+                {
+                    Console.WriteLine(myError.Message);
+                    isValid = false;
+
+                }
+                catch (FormatException myError)
+                {
+                    Console.WriteLine(myError.Message);
+                    isValid = false;
+
+                }
+                finally
+                {
+                    Console.WriteLine("OK");
+
+
+                }
+
+
+
+
+            }
+            while (isValid == false);
+
+        }
+        public static double Divide(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new ArithmeticException("Fuck off");
+            else
+                return (double)numerator / denominator;
+        }
+    }
+}
